6bd97078c2b1ebbf6feb0f93ea3bddebc753e629
janardan1995 <44210489+janardan1995@users.noreply.github.com>
2020-04-15 18:28:53 +0530

    [JD]Add.Create a new branch name as QuentityMeasurement_CelsiusToFarhenheight and push it to remote repository

---
+++ QuentityMeasurementTest/CelsiusToFarhenheithUnitTest.cs
@@ -0,0 +1,203 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="CelsiusToFarhenheithUnitTest.cs" company="Bridgelabz">
+//   Copyright © 2020 Company="BridgeLabz"
+// </copyright>
+// <creator name="Janardan Das"/>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace QuentityMeasurementTest
+{
+    using NUnit.Framework;
+    using QuantityMeasurement_TemperatureCalculation;
+
+    /// <summary>
+    /// CelsiusToFarhenheithUnitTest class
+    /// /// </summary>
+    public class CelsiusToFarhenheithUnitTest
+    {
+        /// <summary>
+        /// Givens the 0 celsius and0 celsius should return true.
+        /// </summary>
+        [Test]
+        public void Given_0Celsius_And0Celsius_ShouldReturnTrue()
+        {
+            Celsius celsius = new Celsius(0);
+            Celsius _celsius = new Celsius(0);
+            var actual = celsius.ConvertedCelsiusValue(_celsius);
+            Assert.IsTrue(actual);
+        }
+
+        /// <summary>
+        /// Givens the null celsius should return false.
+        /// </summary>
+        [Test]
+        public void Given_nullCelsius_ShouldReturnFalse()
+        {
+            Celsius _celsius = new Celsius(11);
+            var actual = _celsius.Equals(null);
+            Assert.IsFalse(actual);
+        }
+
+        /// <summary>
+        /// Fors the celsius reference check should return false.
+        /// </summary>
+        [Test]
+        public void ForCelsius_RefCheck_ShouldReturnFalse()
+        {
+            Celsius _celsius = new Celsius(11);
+            var actual = this.Equals(_celsius);
+            Assert.IsFalse(actual);
+        }
+
+        /// <summary>
+        /// Fors the celsius type check should return true.
+        /// </summary>
+        [Test]
+        public void ForCelsius_TypeCheck_ShouldReturnTrue()
+        {
+            Celsius _celsius = new Celsius(11);
+            Celsius celsius = new Celsius(11);
+            var actual = _celsius.Equals(celsius);
+            Assert.IsTrue(actual);
+        }
+
+        /// <summary>
+        /// Givens the similar celsius values while cheking should return true.
+        /// </summary>
+        [Test]
+        public void GivenSimilar_CelsiusValues_WhileCheking_ShouldReturnTrue()
+        {
+            Celsius _celsius = new Celsius(11);
+            Celsius celsius = new Celsius(11);
+            var actual = _celsius.ConvertedCelsiusValue(celsius);
+            Assert.IsTrue(actual);
+        }
+
+        /// <summary>
+        /// Givens the odd celsius values while cheking should return false.
+        /// </summary>
+        [Test]
+        public void GivenOdd_CelsiusValues_WhileCheking_ShouldReturnFalse()
+        {
+            Celsius _celsius = new Celsius(11);
+            Celsius celsius = new Celsius(110);
+            var actual = _celsius.ConvertedCelsiusValue(celsius);
+            Assert.IsFalse(actual);
+        }
+
+        /// <summary>
+        /// Givens the null fahrenheith should return false.
+        /// </summary>
+        [Test]
+        public void Given_nullFahrenheith_ShouldReturnFalse()
+        {
+            Fahrenheith fahrenheith = new Fahrenheith(11);
+            var actual = fahrenheith.Equals(null);
+            Assert.IsFalse(actual);
+        }
+
+        /// <summary>
+        /// Fors the fahrenheith reference check should return false.
+        /// </summary>
+        [Test]
+        public void ForFahrenheith_RefCheck_ShouldReturnFalse()
+        {
+            Fahrenheith fahrenheith = new Fahrenheith(11);
+            var actual = this.Equals(fahrenheith);
+            Assert.IsFalse(actual);
+        }
+
+        /// <summary>
+        /// Fors the fahrenheith type check should return true.
+        /// </summary>
+        [Test]
+        public void ForFahrenheith_TypeCheck_ShouldReturnTrue()
+        {
+            Fahrenheith fahrenheith = new Fahrenheith(11);
+            Fahrenheith _fahrenheith = new Fahrenheith(13);
+            var actual = fahrenheith.Equals(_fahrenheith);
+            Assert.IsTrue(actual);
+        }
+
+        /// <summary>
+        /// Givens the similar fahrenheith values while cheking should return true.
+        /// </summary>
+        [Test]
+        public void GivenSimilar_FahrenheithValues_WhileCheking_ShouldReturnTrue()
+        {
+            Fahrenheith fahrenheith = new Fahrenheith(11);
+            Fahrenheith _fahrenheith = new Fahrenheith(11);
+            var actual = fahrenheith.ConvertedFahrenheithValue(_fahrenheith);
+            Assert.IsTrue(actual);
+        }
+
+        /// <summary>
+        /// Givens the odd fahrenheith values while cheking should return false.
+        /// </summary>
+        [Test]
+        public void GivenOdd_FahrenheithValues_WhileCheking_ShouldReturnFalse()
+        {
+            Fahrenheith _fahrenheith = new Fahrenheith(11);
+            Fahrenheith fahrenheith = new Fahrenheith(110);
+            var actual = _fahrenheith.ConvertedFahrenheithValue(fahrenheith);
+            Assert.IsFalse(actual);
+        }
+
+        /// <summary>
+        /// Givens the 0 fahrenheith and0 celsius while cheking should return false.
+        /// </summary>
+        [Test]
+        public void Given_0FahrenheithAnd0Celsius_WhileCheking_ShouldReturnFalse()
+        {
+            Fahrenheith fahrenheith = new Fahrenheith(0);
+            Celsius celsius = new Celsius(0);
+
+            CelsiusToFahrenheith CelsiusToFahrenheith = new CelsiusToFahrenheith(celsius, fahrenheith);
+            var actual = CelsiusToFahrenheith.ConvertedCelsiusToFarhenheithValue(celsius);
+            Assert.IsFalse(actual);
+        }
+
+        /// <summary>
+        /// Givens the 1 fahrenheith and1 celsius while cheking should return false.
+        /// </summary>
+        [Test]
+        public void Given_1FahrenheithAnd1Celsius_WhileCheking_ShouldReturnFalse()
+        {
+            Fahrenheith fahrenheith = new Fahrenheith(1);
+            Celsius celsius = new Celsius(1);
+
+            CelsiusToFahrenheith CelsiusToFahrenheith = new CelsiusToFahrenheith(celsius, fahrenheith);
+            var actual = CelsiusToFahrenheith.ConvertedCelsiusToFarhenheithValue(celsius);
+            Assert.IsFalse(actual);
+        }
+
+        /// <summary>
+        /// Givens the 32 fahrenheith 0 celsius while cheking should return true.
+        /// </summary>
+        [Test]
+        public void Given_32Fahrenheith_0Celsius_WhileCheking_ShouldReturnTrue()
+        {
+            Fahrenheith fahrenheith = new Fahrenheith(32);
+            Celsius celsius = new Celsius(0);
+
+            CelsiusToFahrenheith CelsiusToFahrenheith = new CelsiusToFahrenheith(celsius, fahrenheith);
+            var actual = CelsiusToFahrenheith.ConvertedCelsiusToFarhenheithValue(celsius);
+            Assert.IsTrue(actual);
+        }
+
+        /// <summary>
+        /// Givens the fahrenheith celsius while cheking should return false.
+        /// </summary>
+        [Test]
+        public void Given_Fahrenheith_Celsius_WhileCheking_ShouldReturnFalse()
+        {
+
+            Fahrenheith fahrenheith = new Fahrenheith(212);
+            Celsius celsius = new Celsius(100);
+
+            CelsiusToFahrenheith CelsiusToFahrenheith = new CelsiusToFahrenheith(celsius, fahrenheith);
+            var actual = CelsiusToFahrenheith.ConvertedFarhenheithTocelsiusValue(fahrenheith);
+            Assert.IsTrue(actual);
+        }       
+    }
+}
